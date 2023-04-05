1cd580d8e4b5167487b6e831b5a6b6415fa6156e
nmathias2101@gmail.com <nmathias2101@gmail.com>
2023-04-04 17:49:00 -0700

    print sample size & error with best approximation

--- approx-pi.py
+++ approx-pi.py
@@ -3,7 +3,10 @@ import numpy
 import math
 
 # test on different sample sizes & find the most accurate one
-sample_size_lst = [10, 100, 1000, 10000, 25000, 40000]
+sample_size_lst = range(1000, 6000, 1000)
+
+diff_dict = {}
+pi_approx_dict = {}
 
 # circle inside square with side = 1
 for N in sample_size_lst:
@@ -24,7 +27,16 @@ for N in sample_size_lst:
             num_points_inside_circle += 1
 
     pi_approx = 4 * (num_points_inside_circle / float(N))
-    print("Pi is approximately ", pi_approx, "when sample size is ", N)
-
     diff = math.pi - pi_approx
-    print("Difference between approximated pi & built-in value of pi is ", diff)
+
+    #print("Pi is approximately ", pi_approx, "when sample size is ", N)
+    #print("Difference between approximated pi & built-in value of pi is ", diff)
+    diff_dict[N] = diff
+    pi_approx_dict[N] = pi_approx
+
+
+
+# print value of approximated pi with minimum error
+best_sample_size = min(diff_dict, key=diff_dict.get)
+print(pi_approx_dict[best_sample_size])
+print(min(diff_dict.items(), key=lambda x: x[1]))
